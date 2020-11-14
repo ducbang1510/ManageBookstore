c701733a39d17086b8100efe320cd76e5d816ab1
ducbang1510 <1851050006bang@ou.edu.vn>
2020-11-14 21:52:50 +0700

    update

--- bookshopdb.sql
+++ bookshopdb.sql
@@ -65,55 +65,55 @@ LOCK TABLES `book` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `bookauthor`
+-- Table structure for table `book_author`
 --
 
-DROP TABLE IF EXISTS `bookauthor`;
+DROP TABLE IF EXISTS `book_author`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bookauthor` (
+CREATE TABLE `book_author` (
   `book_id` int NOT NULL,
   `author_id` int NOT NULL,
   PRIMARY KEY (`book_id`,`author_id`),
   KEY `author_id` (`author_id`),
-  CONSTRAINT `bookauthor_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
-  CONSTRAINT `bookauthor_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+  CONSTRAINT `book_author_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  CONSTRAINT `book_author_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bookauthor`
+-- Dumping data for table `book_author`
 --
 
-LOCK TABLES `bookauthor` WRITE;
-/*!40000 ALTER TABLE `bookauthor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bookauthor` ENABLE KEYS */;
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `bookcate`
+-- Table structure for table `book_cate`
 --
 
-DROP TABLE IF EXISTS `bookcate`;
+DROP TABLE IF EXISTS `book_cate`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bookcate` (
+CREATE TABLE `book_cate` (
   `book_id` int NOT NULL,
   `category_id` int NOT NULL,
   PRIMARY KEY (`book_id`,`category_id`),
   KEY `category_id` (`category_id`),
-  CONSTRAINT `bookcate_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
-  CONSTRAINT `bookcate_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+  CONSTRAINT `book_cate_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  CONSTRAINT `book_cate_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bookcate`
+-- Dumping data for table `book_cate`
 --
 
-LOCK TABLES `bookcate` WRITE;
-/*!40000 ALTER TABLE `bookcate` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bookcate` ENABLE KEYS */;
+LOCK TABLES `book_cate` WRITE;
+/*!40000 ALTER TABLE `book_cate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book_cate` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -140,42 +140,70 @@ LOCK TABLES `category` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `detailinventoryreport`
+-- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `detailinventoryreport`;
+DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `detailinventoryreport` (
-  `report_id` int NOT NULL,
-  `book_id` int NOT NULL,
-  `quantity_at_first` int DEFAULT NULL,
-  `quantity_at_the_end` int DEFAULT NULL,
-  `arise` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`report_id`,`book_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `detailinventoryreport_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `inventoryreport` (`id`),
-  CONSTRAINT `detailinventoryreport_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `debt` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `detailinventoryreport`
+-- Dumping data for table `customer`
 --
 
-LOCK TABLES `detailinventoryreport` WRITE;
-/*!40000 ALTER TABLE `detailinventoryreport` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detailinventoryreport` ENABLE KEYS */;
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `inventoryreport`
+-- Table structure for table `debt_collection_note`
 --
 
-DROP TABLE IF EXISTS `inventoryreport`;
+DROP TABLE IF EXISTS `debt_collection_note`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `inventoryreport` (
+CREATE TABLE `debt_collection_note` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `collection_date` date DEFAULT NULL,
+  `proceeds` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `debt_collection_note_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `debt_collection_note_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `debt_collection_note`
+--
+
+LOCK TABLES `debt_collection_note` WRITE;
+/*!40000 ALTER TABLE `debt_collection_note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `debt_collection_note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dept_report`
+--
+
+DROP TABLE IF EXISTS `dept_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dept_report` (
   `id` int NOT NULL AUTO_INCREMENT,
   `report_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -183,12 +211,205 @@ CREATE TABLE `inventoryreport` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `inventoryreport`
+-- Dumping data for table `dept_report`
 --
 
-LOCK TABLES `inventoryreport` WRITE;
-/*!40000 ALTER TABLE `inventoryreport` DISABLE KEYS */;
-/*!40000 ALTER TABLE `inventoryreport` ENABLE KEYS */;
+LOCK TABLES `dept_report` WRITE;
+/*!40000 ALTER TABLE `dept_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dept_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_dept_report`
+--
+
+DROP TABLE IF EXISTS `detail_dept_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detail_dept_report` (
+  `report_id` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `money_before` float DEFAULT NULL,
+  `money_after` float DEFAULT NULL,
+  `arise` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`report_id`,`customer_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `detail_dept_report_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `dept_report` (`id`),
+  CONSTRAINT `detail_dept_report_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_dept_report`
+--
+
+LOCK TABLES `detail_dept_report` WRITE;
+/*!40000 ALTER TABLE `detail_dept_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_dept_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_inventory_report`
+--
+
+DROP TABLE IF EXISTS `detail_inventory_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detail_inventory_report` (
+  `report_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `quantity_before` int DEFAULT NULL,
+  `quantity_after` int DEFAULT NULL,
+  `arise` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`report_id`,`book_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `detail_inventory_report_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `inventory_report` (`id`),
+  CONSTRAINT `detail_inventory_report_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_inventory_report`
+--
+
+LOCK TABLES `detail_inventory_report` WRITE;
+/*!40000 ALTER TABLE `detail_inventory_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_inventory_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_invoice`
+--
+
+DROP TABLE IF EXISTS `detail_invoice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detail_invoice` (
+  `invoice_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `quantity` int DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`invoice_id`,`book_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `detail_invoice_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`),
+  CONSTRAINT `detail_invoice_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_invoice`
+--
+
+LOCK TABLES `detail_invoice` WRITE;
+/*!40000 ALTER TABLE `detail_invoice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_invoice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_received_note`
+--
+
+DROP TABLE IF EXISTS `detail_received_note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detail_received_note` (
+  `note_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`note_id`,`book_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `detail_received_note_ibfk_1` FOREIGN KEY (`note_id`) REFERENCES `received_note` (`id`),
+  CONSTRAINT `detail_received_note_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_received_note`
+--
+
+LOCK TABLES `detail_received_note` WRITE;
+/*!40000 ALTER TABLE `detail_received_note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_received_note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory_report`
+--
+
+DROP TABLE IF EXISTS `inventory_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory_report` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `report_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory_report`
+--
+
+LOCK TABLES `inventory_report` WRITE;
+/*!40000 ALTER TABLE `inventory_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoice`
+--
+
+DROP TABLE IF EXISTS `invoice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_of_invoice` date DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `paid` float DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `invoice_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice`
+--
+
+LOCK TABLES `invoice` WRITE;
+/*!40000 ALTER TABLE `invoice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `received_note`
+--
+
+DROP TABLE IF EXISTS `received_note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `received_note` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_received` date DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `received_note_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `received_note`
+--
+
+LOCK TABLES `received_note` WRITE;
+/*!40000 ALTER TABLE `received_note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `received_note` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -232,4 +453,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-13 16:52:43
+-- Dump completed on 2020-11-14 21:48:50
--- manageApp/models.py
+++ manageApp/models.py
@@ -10,50 +10,103 @@ class SaleBase(db.Model):
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), nullable=False)
 
     def __str__(self):
         return self.name
 
 
-#Bảng thể loại
-class Category(SaleBase):
-    __tablename__ = 'category'
+#Bảng khách hàng
+class Customer(SaleBase):
+    __tablename__ = 'customer'
 
-    books = relationship('Book', secondary='bookcate')
+    name = Column(String(50), nullable=False)
+    address = Column(String(255))
+    phone = Column(String(20), nullable=True)
+    email = Column(String(50), nullable=True)
+    debt = Column(Float, default=0)
+    deptReports = relationship('DeptReport', secondary='detail_dept_report')
 
 
 #Bảng sách
 class Book(SaleBase):
     __tablename__ = 'book'
 
+    name = Column(String(50), nullable=False)
     description = Column(String(255))
     image = Column(String(255))
     price = Column(Float, default=0)
-    categories = relationship('Category', secondary='bookcate')
-    authors = relationship('Author', secondary='bookauthor')
-    invenReports = relationship('InventoryReport', secondary='detailInventoryReport')
+    categories = relationship('Category', secondary='book_cate')
+    authors = relationship('Author', secondary='book_author')
+    invenReports = relationship('InventoryReport', secondary='detail_inventory_report')
+    invoices = relationship('Invoice', secondary='detail_invoice')
+    receivedNotes = relationship('ReceivedNote', secondary='detail_received_note')
+
+
+#Bảng thể loại
+class Category(SaleBase):
+    __tablename__ = 'category'
+
+    name = Column(String(50), nullable=False)
+    books = relationship('Book', secondary='book_cate')
 
 
 #Bảng tác giả
 class Author(SaleBase):
     __tablename__ = 'author'
 
-    books = relationship('Book', secondary='bookauthor')
+    name = Column(String(50), nullable=False)
+    books = relationship('Book', secondary='book_author')
+
+
+#Bảng hóa đơn
+class Invoice(SaleBase):
+    __tablename__ = 'invoice'
+
+    date_of_invoice = Column(Date, default=datetime.now())
+    total = Column(Float, default=0)
+    paid = Column(Float, default=0)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    books = relationship('Book', secondary='detail_invoice')
+
+
+#Bảng phiếu thu tiền
+class DebtCollectionNote(SaleBase):
+    __tablename__ = 'debt_collection_note'
+
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    collection_date = Column(Date, default=datetime.now())
+    proceeds = Column(Float, default=0)
+
+
+#Bảng phiếu nhập sách
+class ReceivedNote(SaleBase):
+    __tablename__ = 'received_note'
+
+    date_received = Column(Date, default=datetime.now())
+    user_id = Column(Integer, ForeignKey('user.id'))
+    books = relationship('Book', secondary='detail_received_note')
 
 
 #Bảng báo cáo tồn
-class InventoryReport(db.Model):
-    __tablename__ = 'inventoryReport'
+class InventoryReport(SaleBase):
+    __tablename__ = 'inventory_report'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
     report_date = Column(Date, default=datetime.now())
-    books = relationship('Book', secondary='detailInventoryReport')
+    books = relationship('Book', secondary='detail_inventory_report')
+
+
+#Bảng báo cáo công nợ
+class DeptReport(SaleBase):
+    __tablename__ = 'dept_report'
+
+    report_date = Column(Date, default=datetime.now())
 
 
 #Bảng sách-thể loại
 class BookCate(db.Model):
-    __tablename__ = 'bookcate'
+    __tablename__ = 'book_cate'
 
     book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
     category_id = Column(Integer, ForeignKey('category.id'), primary_key=True)
@@ -61,32 +114,64 @@ class BookCate(db.Model):
 
 #Bảng sách-tác giả
 class BookAuthor(db.Model):
-    __tablename__ = 'bookauthor'
+    __tablename__ = 'book_author'
 
     book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
     author_id = Column(Integer, ForeignKey('author.id'), primary_key=True)
 
 
+#Bảng chi tiết hóa đơn
+class DetailInvoice(db.Model):
+    __tablename__ = 'detail_invoice'
+
+    invoice_id = Column(Integer, ForeignKey('invoice.id'), primary_key=True)
+    book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
+    quantity = Column(Integer, default=0)
+    price = Column(Float, default=0)
+
+
+#Bảng chi tiết phiếu nhập sách
+class DetailReceivedNote(db.Model):
+    __tablename__ = 'detail_received_note'
+
+    note_id = Column(Integer, ForeignKey('received_note.id'), primary_key=True)
+    book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
+    quantity = Column(Integer, default=0)
+
+
 #Bảng chi tiết báo cáo tồn
 class DetailInventoryReport(db.Model):
-    __tablename__ = 'detailInventoryReport'
+    __tablename__ = 'detail_inventory_report'
 
-    report_id = Column(Integer, ForeignKey('inventoryReport.id'), primary_key=True)
+    report_id = Column(Integer, ForeignKey('inventory_report.id'), primary_key=True)
     book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
-    quantity_at_first = Column(Integer, default=0)
-    quantity_at_the_end = Column(Integer, default=0)
+    quantity_before = Column(Integer, default=0)
+    quantity_after = Column(Integer, default=0)
+    arise = Column(String(255))
+
+
+#Bảng chi tiết báo cáo công nợ
+class DetailDeptReport(db.Model):
+    __tablename__ = 'detail_dept_report'
+
+    report_id = Column(Integer, ForeignKey('dept_report.id'), primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customer.id'), primary_key=True)
+    money_before = Column(Float, default=0)
+    money_after = Column(Float, default=0)
     arise = Column(String(255))
 
 
 class UserRole(UserEnum):
     USER = 1
     ADMIN = 2
+    MANAGER = 3
 
 
 #Bảng user
 class User(SaleBase, UserMixin):
     __tablename__ = 'user'
 
+    name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=True)
     username = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
